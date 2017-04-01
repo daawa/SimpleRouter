7e51127026d30eabfad67823eed51fab0002b2ef
daawa <mayzzw@126.com>
2017-04-01 16:00:18 +0800

    check if notification enabled

--- app/src/main/java/simple/router/sample/MainActivity.java
+++ app/src/main/java/simple/router/sample/MainActivity.java
@@ -7,9 +7,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.List;
 import java.util.Map;
@@ -40,21 +44,39 @@ public class MainActivity extends AppCompatActivity {
                 new TargetIntent() {
                     @Override
                     public Intent intent(Context context, List<String> extractedPathParams, Map<String, String> extractedQueryParams) {
-                        Intent intent = new Intent(getApplicationContext(),TestActivity.class);
+                        Intent intent = new Intent(getApplicationContext(), TestActivity.class);
                         intent.putExtra(TestActivity.PATH_KEY1, extractedPathParams.get(0));
                         intent.putExtra(TestActivity.PATH_KEY2, extractedPathParams.get(1));
                         intent.putExtra(TestActivity.QUERY_KEY, extractedQueryParams.get(TestActivity.QUERY_KEY));
                         return intent;
                     }
                 });
+
+        findViewById(R.id.send_notification).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendNotification(getApplicationContext());
+            }
+        });
+
+        findViewById(R.id.check_enabled).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean enabled = isNotificationEnabled();
+
+                //if disabled , toast can't show out either on MIUI
+                Toast.makeText(getApplicationContext(), enabled? " enabled:" : "disabled", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    public static void sendNotification(Context context) {
+    public void sendNotification(Context context) {
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
         String title = "Test Message";
         builder.setTicker(title);
         builder.setContentTitle(title);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
         builder.setContentText("TEST");
         builder.setAutoCancel(true);
         builder.setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher)).setColor(Color.parseColor("#EAA935"));
@@ -69,6 +91,17 @@ public class MainActivity extends AppCompatActivity {
 
         builder.setDefaults(NotificationCompat.DEFAULT_ALL);
         Notification notification = builder.build();
-        notificationManager.notify(notification_id++, notification);
+        notificationManager.notify("myApp", notification_id++, notification);
+
+    }
+
+    private boolean isNotificationEnabled() {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return notificationManager.areNotificationsEnabled();
+        } else {
+            return NotificationManagerCompat.from(this).areNotificationsEnabled();
+        }
+
     }
 }
